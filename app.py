fa7bd55efaca130c7936b618c4dd2c508b03099b
daul <daul.otaruly@gmail.com>
2025-11-10 00:29:42 +0200

    dotenv, exit, chunk sources

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import os
 from retriever import load_files, chunk_files, chunks_to_embeddings
 from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
 from langchain.chains import RetrievalQA
+from dotenv import load_dotenv
+
+load_dotenv()
 
 docs = load_files("data")
 chunks = chunk_files(docs)
@@ -10,6 +13,7 @@ retriever = chunks_to_embeddings(chunks)
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     temperature=0.5,
+    google_api_key=os.getenv("GOOGLE_API_KEY")
 )
 
 qa_chain = RetrievalQA.from_chain_type(
@@ -20,5 +24,10 @@ qa_chain = RetrievalQA.from_chain_type(
 
 while True:
     query = input("ask a question:")
+    if query.lower() == "exit":
+        break
     result = qa_chain.invoke({"query": query})
     print("answer:", result["result"])
+    print("sources:")
+    for i, doc in enumerate(result["source_documents"], 1):
+        print(f" - Chunk {i}: {doc.metadata.get('source', 'Unknown')}")
